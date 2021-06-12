85b2ecfa7ab3ab4523a4dadee4c368d77abbbe7b
Rahul Bhardwaj <57061076+rahulgitx@users.noreply.github.com>
2021-06-12 17:46:46 +0530

    Update server.py

--- server.py
+++ server.py
@@ -5,30 +5,30 @@ import pickle
 import cv2
 
 cap = cv2.VideoCapture(0)
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-ip="127.0.0.1"
-port=1234
-socket_address = (ip,port)
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)                                    # creating socket
+ip="127.0.0.1"                                                                          # own IP 
+port=1234                                                                               # own port
+socket_address = (ip,port)                                                                
 
 try:
-    s.bind((ip,port))
+    s.bind((ip,port))                                                                  # binding port
     print("Binded")
-    s.listen()
+    s.listen()                                                                         # waiting for client to connect
     print("LISTENING AT:",socket_address)
 
     o , addr = s.accept()
     print(o)
     print("Connected to {}".format(addr))
-    while True:
-        ret , photo = cap.read()
-        ret, buffer = cv2.imencode('.jpg',photo)
-        bytedata = pickle.dumps(buffer)
-        o.send(bytedata)
-
-        x = o.recv(1000000)
-
-        data = pickle.loads(x)
-        data = cv2.imdecode(data,cv2.IMREAD_COLOR)
+    while True:                                                                       # -----------sending part ------------
+        ret , photo = cap.read()                                                      #
+        ret, buffer = cv2.imencode('.jpg',photo)                                      #  compressing the .jpf file and storing it in the memory buffer that is resized to fit the result
+        bytedata = pickle.dumps(buffer)                                               #  pickle serialises the object file. Serializing and saving it in bytedata
+        o.send(bytedata)                                                              # sending to client                                                        
+                                                                                      # --------------receiving part----------
+        x = o.recv(1000000)                                                           # receiving bytes from the socket
+  
+        data = pickle.loads(x)                                                        # serializing back the deserialized data that was received in x.
+        data = cv2.imdecode(data,cv2.IMREAD_COLOR)                                    # doing reverse of what is done in line 27. Uncompressing and getting the info from memory buffer
         if data is not None :
             cv2.imshow('serverphoto',data)
             if cv2.waitKey(10) == 13 :
